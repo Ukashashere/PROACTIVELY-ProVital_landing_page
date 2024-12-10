8d1f96b53787e1dd2e2287f14526bbec80f31a18
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-10 16:20:36 +0530

    6.5

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,28 +6,6 @@ import React from 'react';
 const Home = () => {
   return(                                 /*This thing is the HTML part*/
       <div className="home-container">
-          <div className="">         {/*typo was there*/}
-            <div className="">   {/*typo was here too*/}
-                       {/*Now the Home Error got fixed*/}
-            </div>
-            <div className='home-text-section'>
-              <h1 className='primary-heading'>
-                Book an appointment with
-                lifestyle medicine experts
-              </h1>
-              <p className='primary-text'>
-                Healthy switchefr chefs do all the prep work, like peeding,
-                chopping
-                & marianting, so you can cook a fresh food.
-              </p>
-              <button className='secondary-button'>
-                
-              </button>
-            </div>
-            <div className="home-image-section">    {/*this was not present*/}
-
-            </div>
-          </div>
       </div>
   );  
 };
